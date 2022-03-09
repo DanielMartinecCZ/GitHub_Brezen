33999081a0c0ab20dc6438a5f9d896f539cac8fa
10dan <10dan@DESKTOP-HA9832H>
2022-03-09 17:33:28 +0100

    Přidejte soubory projektu.

---
+++ RepositarGitHubProjekt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RepositarGitHubProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = false;
+            bool y = false;
+            while(!x || !y)
+            {
+                    Console.Clear();
+                    Console.Write("Zadej první číslo: ");
+                    x = int.TryParse(Console.ReadLine(), out int prvniCislo);
+                    Console.Write("Zadej druhé číslo: ");
+                    y = int.TryParse(Console.ReadLine(), out int druheCislo);
+                    Console.WriteLine($"Větší číslo: {Math.Max(prvniCislo, druheCislo)}, Menší číslo: {Math.Min(prvniCislo, druheCislo)}");
+                
+            }
+            
+           
+
+
+        }
+    }
+}
