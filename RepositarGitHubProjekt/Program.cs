2f5dccf6b7a5f6143d77d1000d5d667fe6c81329
Daniel Martinec <10dan10@email.cz>
2022-03-09 18:08:47 +0100

    Vytvoření nové větve pro vývoj aplikace

--- RepositarGitHubProjekt/Program.cs
+++ RepositarGitHubProjekt/Program.cs
@@ -8,18 +8,18 @@ namespace RepositarGitHubProjekt
         {
             bool x = false;
             bool y = false;
-            while(!x || !y)
+            while (!x || !y)
             {
-                    Console.Clear();
-                    Console.Write("Zadej první číslo: ");
-                    x = int.TryParse(Console.ReadLine(), out int prvniCislo);
-                    Console.Write("Zadej druhé číslo: ");
-                    y = int.TryParse(Console.ReadLine(), out int druheCislo);
-                    Console.WriteLine($"Větší číslo: {Math.Max(prvniCislo, druheCislo)}, Menší číslo: {Math.Min(prvniCislo, druheCislo)}");
-                
+                Console.Clear();
+                Console.Write("Zadej první číslo: ");
+                x = int.TryParse(Console.ReadLine(), out int prvniCislo);
+                Console.Write("Zadej druhé číslo: ");
+                y = int.TryParse(Console.ReadLine(), out int druheCislo);
+                Console.WriteLine((prvniCislo == druheCislo) ? "Obě čísla jsou stejně velká" : $"Větší číslo: {Math.Max(prvniCislo, druheCislo)}, Menší číslo: {Math.Min(prvniCislo, druheCislo)}");
+
             }
-            
-           
+
+
 
 
         }
